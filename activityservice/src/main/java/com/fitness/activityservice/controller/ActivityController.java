c92e7d9be3cb030096d62b8d077b40a364ba78f6
Ashutosh <ashutosh10teotia@gmail.com>
2025-06-07 23:21:38 +0530

    creating few more endpoints for activity service

--- activityservice/src/main/java/com/fitness/activityservice/controller/ActivityController.java
+++ activityservice/src/main/java/com/fitness/activityservice/controller/ActivityController.java
@@ -6,10 +6,9 @@ import com.fitness.activityservice.model.Activity;
 import com.fitness.activityservice.service.ActivityService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/activities")
@@ -21,5 +20,15 @@ public class ActivityController {
     @PostMapping
     public ResponseEntity<ActivityResponse> trackActivity(@RequestBody ActivityRequest request){
         return ResponseEntity.ok(activityService.trackActivity(request));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ActivityResponse>> getUserActivity(@RequestHeader("X-user-ID") String userId){
+        return ResponseEntity.ok(activityService.getUserActivity(userId));
+    }
+
+    @GetMapping("/{activityId}")
+    public ResponseEntity<ActivityResponse> getActivity(@PathVariable String activityId){
+        return ResponseEntity.ok(activityService.getActivityById(activityId));
     }
 }
